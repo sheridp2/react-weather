efc258e10e050bddc282e86c2326b7c1e53d5491
Patrick Sheriddan <sheridp2@gmail.com>
2020-09-13 21:32:24 -0700

    season selector, icons, styling added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  state={ lat: null, errorMessage: ''}
 
-    this.state={ lat: null, errorMessage: '' };
-
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+
     )
   }
 
   render() {
-    return(
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
 
-    )
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+    return <div>Loading...</div>
+
   }
 }
 
